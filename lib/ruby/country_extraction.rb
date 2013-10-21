7ebfd5e19c3ec1b99443c3f4a07454ea0f967a0d
tadl <tadl@gmx.de>
2013-10-22 00:55:10 +0200

    countries corrected

--- lib/ruby/country_extraction.rb
+++ lib/ruby/country_extraction.rb
@@ -15,14 +15,15 @@ f.gsub!(/\r\n?/, "\n")
 f.each_line do |line|
   #puts line
   #r = line.to_s.match(/[.]*\/([A-Za-z]*)>$/)
-  line =~ (/[.]*\/([^\/]*)>$/)
+  line =~ (/[.]*\/([\w]*)>$/)
   #puts r.inspect
-  countries << $1 
+  countries << $1 if $1 != nil
 end
 
 puts countries[1..10].join("|")
 puts countries.length
 countries.uniq!
+countries.sort!
 puts countries.length
 
 out = File.write 'country_names',  countries.join("\n")
